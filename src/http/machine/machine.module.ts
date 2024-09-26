84c61776ca2ee187294f400125ba625e554464b7
Cokke <jorgerandomgamer@gmail.com>
2024-09-26 19:41:52 -0300

    typado Ms-Questionnarie

--- src/http/machine/machine.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MachineService } from './machine.service';
-import { MachineController } from './machine.controller';
-
-@Module({
-  controllers: [MachineController],
-  providers: [MachineService],
-})
-export class MachineModule {}
